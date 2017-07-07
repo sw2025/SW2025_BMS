c3d0eac665121a3b0746237d5ad495860de89046
1874207723 <18742077232@qq.com>
2017-07-07 16:38:10 +0800

    7-7worning

--- app/Http/Controllers/SupplyController.php
+++ app/Http/Controllers/SupplyController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class SupplyController extends Controller
 {
@@ -14,12 +15,68 @@ class SupplyController extends Controller
      * @return mixed
      */
     public function index(){
-        return view("supply.index");
+        $status = empty($_GET['status']) ? 'all' : $_GET['status'];
+        $datas = DB::table("t_n_need")
+            ->leftJoin("t_u_user", "t_u_user.userid", "=", "t_n_need.userid")
+            ->leftJoin("t_n_needverify", "t_n_needverify.needid", "=", "t_n_need.needid")
+            ->select("t_n_needverify.configid", "t_n_needverify.verifytime", "t_n_needverify.remark", "t_u_user.phone", "t_u_user.name", "t_n_need.*")
+            ->orderBy("t_n_needverify.verifytime", "desc")
+            ->where('is_deal','0');
+        switch($status){
+            case 'all':
+                $datas = $datas->whereIn("configid", [1, 3])->paginate(3);
+                break;
+            case 'wait':
+                $datas = $datas->where('configid',1)->paginate(3);
+                break;
+            case 'fail':
+                $datas = $datas->where("configid", 3)->paginate(3);
+                break;
+        }
+        return view("supply.index", compact('datas','status'));
     }
 
-    public  function update(){
-        return view("supply.update");
+    /**供求信息详情
+     * @param $supply_id  供求表中id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public  function update($supply_id){
+
+        $datas = DB::table("t_n_need")
+            ->leftJoin("t_u_user", "t_u_user.userid", "=", "t_n_need.userid")
+            ->leftJoin("t_n_needverify", "t_n_needverify.needid", "=", "t_n_need.needid")
+            ->select("t_n_needverify.configid", "t_n_needverify.verifytime", "t_n_needverify.remark", "t_u_user.phone", "t_u_user.name", "t_n_need.*")
+            ->where('t_n_need.needid',$supply_id)
+            ->orderBy("created_at", "desc")
+            ->first();
+        return view("supply.update",compact('datas'));
     }
 
-
+    /**更改供求审核
+     * @param Request $request
+     * @return string
+     */
+    public function changeSupply(Request $request)
+    {
+        $datas = $request->input();
+        $update = DB::table('t_n_needverify')->where('needid' , $datas['supply_id'])
+            ->update(
+                ['is_deal' => 1]
+            );
+        $result=DB::table("t_n_needverify")
+            ->insert([
+                'needid' => $datas['supply_id'],
+                "configid"=>$datas['config_id'],
+                'verifytime' => date('Y-m-d H:i:s',time()),
+                "remark"=>!empty($datas['remark'])?$datas['remark']:"",
+                "updated_at"=>date("Y-m-d H:i:s",time()),
+                "created_at"=>date("Y-m-d H:i:s",time())
+            ]);
+        if ($result || $update) {
+            return json_encode(['errorMsg' => 'success']);
+        } else {
+            return json_encode(['errorMsg' => 'error']);
+        }
+    }
 }
+
--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -6,22 +6,79 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class WorkController extends Controller
 {
     /**办事审核
      * @return mixed
      */
-    public function index(){
-        return view("work.index");
+    public function index($action = 'all'){
+
+        $datas = DB::table('t_e_event')
+            ->leftJoin('view_userrole','view_userrole.userid', '=','t_e_event.userid')
+            ->leftJoin('t_u_enterprise','t_u_enterprise.enterpriseid', '=','view_userrole.enterpriseid')
+            ->leftJoin('t_u_expert','t_u_expert.expertid' ,'=' ,'view_userrole.expertid')
+            ->leftJoin('t_e_eventverify','t_e_eventverify.eventid' ,'=' ,'t_e_event.eventid')
+            ->leftJoin('t_u_user','t_e_event.userid' ,'=' ,'t_u_user.userid')
+            ->select('t_e_event.*','view_userrole.role','t_u_enterprise.enterprisename','t_u_expert.expertname','t_u_user.phone','t_e_eventverify.verifytime','t_e_eventverify.configid')
+            ->orderBy('t_e_eventverify.verifytime','desc')
+            ->where('is_deal',0);
+        switch ($action) {
+            case 'all':
+                $datas = $datas->whereIn("configid", [1,2,3])->paginate(3);
+                break;
+            case 'wait':
+                $datas = $datas->where("configid", 1)->paginate(3);
+                break;
+            case 'fail':
+                $datas = $datas->where("configid", 3)->paginate(3);
+                break;
+            case 'wput':
+                $datas = $datas->where("configid", 2)->paginate(3);
+                break;
+        }
+        return view("work.index",compact('datas','action'));
     }
 
     /**
      * 办事审核详情
      * @return mixed
      */
-    public function update(){
-        return view("work.update");
+    public function update($event_id){
+        $datas = DB::table('t_e_event')
+            ->leftJoin('view_userrole','view_userrole.userid', '=','t_e_event.userid')
+            ->leftJoin('t_u_enterprise','t_u_enterprise.enterpriseid', '=','view_userrole.enterpriseid')
+            ->leftJoin('t_u_expert','t_u_expert.expertid' ,'=' ,'view_userrole.expertid')
+            ->leftJoin('t_e_eventverify','t_e_eventverify.eventid' ,'=' ,'t_e_event.eventid')
+            ->leftJoin('t_u_user','t_e_event.userid' ,'=' ,'t_u_user.userid')
+            ->select('t_u_enterprise.brief as desc1','t_u_expert.brief as desc2','t_e_event.*','view_userrole.role','t_u_enterprise.enterprisename','t_u_expert.expertname','t_u_user.phone','t_e_eventverify.verifytime','t_e_eventverify.configid')
+            ->orderBy('t_e_eventverify.verifytime','desc')
+            ->where("t_e_event.eventid", $event_id)
+            ->first();
+        return view("work.update",compact('datas'));
+    }
+
+    public function changeEvent(Request $request)
+    {
+        $datas = $request->input();
+        $update = DB::table('t_e_eventverify')->where('eventid' , $datas['event_id'])
+            ->update(
+                ['is_deal' => 1]
+            );
+        $result=DB::table("t_e_eventverify")
+            ->insert([
+                'eventid'    => $datas['event_id'],
+                "configid"   => $datas['config_id'],
+                'verifytime' => date('Y-m-d H:i:s',time()),
+                "remark"     => !empty($datas['remark']) ? $datas['remark'] : "",
+                "updated_at" => date("Y-m-d H:i:s",time()),
+            ]);
+        if ($result || $update) {
+            return json_encode(['errorMsg' => 'success']);
+        } else {
+            return json_encode(['errorMsg' => 'error']);
+        }
     }
 
   
