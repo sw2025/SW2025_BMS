fcf386349a703f13b5a3d9bd02841a3b633cbb1a
jiang <192793660@qq.com>
2017-10-10 17:17:54 +0800

    10/10

--- resources/views/supply/detail.blade.php
+++ resources/views/supply/detail.blade.php
@@ -9,7 +9,7 @@
             </ol>
             <div class="section-body contain-lg change-pwd">
                 <div class="container-fluid details-bg">
-                    <div class="col-md-12 details-tit"><h1 class="cert-company">【{{$datas->needtype}}】@if(!empty($datas->expertname)){{$datas->enterprisename}} @else {{$datas->expertname}} @endif</h1></div>
+                    <div class="col-md-12 details-tit"><h1 class="cert-company">【{{$datas->needtype}}】@if($datas->needtype=='企业'){{$datas->enterprisename}} @else {{$datas->expertname}} @endif</h1></div>
                     <div class="col-md-6 details-tit"><span class="cert-industry">{{$datas->needtime}}</span></div>
                     <div class="col-md-6 details-tit"><span class="cert-industry">联系电话：{{$datas->phone}}</span></div>
                     <div class="col-md-6 details-tit"><p class="cert-industry"><span style="font-size: 14px;font-weight: bold;">需求分类：</span>{{$datas->domain1}}/{{$datas->domain2}}</p></div>
